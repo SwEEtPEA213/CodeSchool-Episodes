30e52f4ece2da69fae7f74489bf413ede7ca2114
Gregg Pollack <Gregg@EnvyLabs.com>
2011-05-09 14:56:47 -0400

    Before and after

---
+++ 001-CSS3Buttons/demo-before/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '6bc301cd151580f11b669d22f96baf3f821f263bbe2ca7082dd578a8820999d75b30816fd9cf98aeb09095645a08ce88763a3f85e5eb9ba34cfd82a8b01f0bc9'
---
+++ 001-CSS3Buttons/demo-before/db/migrate/20110509182808_create_mailing_lists.rb
@@ -0,0 +1,13 @@
+class CreateMailingLists < ActiveRecord::Migration
+  def self.up
+    create_table :mailing_lists do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :mailing_lists
+  end
+end
